96aaaceddd9fc509957bf53b6193447893f943c2
RQUST <rqust@yandex.com>
2024-01-28 16:36:05 +0200

    Create MigLayout.java

---
+++ src/MigLayout.java
@@ -0,0 +1,18 @@
+import net.miginfocom.swing.MigLayout;
+import javax.swing.*;
+
+public class MiGLayout {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MiG Layout Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel(new MigLayout());
+        panel.add(new JButton("Button 1"));
+        panel.add(new JButton("Button 2"));
+        panel.add(new JButton("Button 3"));
+
+        frame.getContentPane().add(panel);
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+    }
+}

680975b4616fef064e78b7c8d4510e1afbe7aa56
RQUST <rqust@yandex.com>
2024-01-30 18:55:40 +0200

    MyFrame ADD JCheckBox

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -6,25 +6,36 @@ public class MyFrame extends JFrame {
     public MyFrame() {
         setLayout(new MigLayout());
 
-        JLabel rowLabel = new JLabel("Rows:");
+        // Создаем панель и устанавливаем для нее MigLayout
+        JPanel panel = new JPanel(new MigLayout());
+
+        JLabel rowLabel = new JLabel("Строк:");
         JSpinner rowSpinner = new JSpinner();
 
-        JLabel colLabel = new JLabel("Columns:");
+        JLabel colLabel = new JLabel("Столбцов:");
         JSpinner colSpinner = new JSpinner();
+
+        JCheckBox addRowHeaderCheckbox = new JCheckBox("Добавить строку заголовка");
+        JCheckBox addColHeaderCheckbox = new JCheckBox("Добавить столбец заголовков");
 
         JButton closeButton = new JButton("Закрыть");
         closeButton.addActionListener(e -> dispose());
 
-        // Добавляем компоненты с использованием MigLayout-констрейнтов
-        add(rowLabel, "align trailing");
-        add(rowSpinner, "width 300!");
-        add(colLabel, "align trailing");
-        add(colSpinner, "width 300!, wrap");
-        add(closeButton, "span 2, align center");
+        // Добавляем компоненты на панель с использованием MigLayout-констрейнтов
+        panel.add(rowLabel, "align trailing");
+        panel.add(rowSpinner, "width 300!");
+        panel.add(colLabel, "align trailing");
+        panel.add(colSpinner, "width 300!, wrap");
+        panel.add(addRowHeaderCheckbox, "span 2, wrap");
+        panel.add(addColHeaderCheckbox, "span 2, wrap");
+        panel.add(closeButton, "span 2, align center");
+
+        // Добавляем панель на JFrame
+        add(panel, "wrap, grow");
 
         setSize(1000, 500);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Для корректного завершения приложения
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 }
--- src/StartApplication.java
+++ src/StartApplication.java
@@ -31,7 +31,7 @@ public class StartApplication {
         JMenu fileMenu = new JMenu("Файл");
         JMenuItem startMenuItem = new JMenuItem("Старт");
 
-        startMenuItem.addActionListener(e -> { 
+        startMenuItem.addActionListener(e -> {
             MyFrame myFrame = new MyFrame();
         });
 

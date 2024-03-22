f71f75705a3413e4f99371737a08bef11d70dc27
RQUST <rqust@yandex.com>
2024-03-22 11:37:18 +0200

    Update Aggregator.java

--- src/Aggregator.java
+++ src/Aggregator.java
@@ -1,14 +1,13 @@
-import javax.swing.table.AbstractTableModel;
-import java.util.Arrays;
-
 public interface Aggregator {
     void addValue(double value);
+
     double getResult();
+
     void reset();
 }
 
 // Сумма чисел
-class SumAgg implements Aggregator{
+class SumAgg implements Aggregator {
 
     private double res = 0;
 
@@ -26,4 +25,138 @@ class SumAgg implements Aggregator{
     public void reset() {
         res = 0d;
     }
+}
+
+// Среднее значение
+class AverageAgg implements Aggregator {
+
+    private double sum = 0;
+    private double count = 0;
+
+    @Override
+    public void addValue(double value) {
+
+
+        sum += value;
+        count++;
+
+    }
+
+    @Override
+    public double getResult() {
+        if (count == 0) {
+            return 0;
+        }
+        return sum / count;
+    }
+
+    @Override
+    public void reset() {
+        sum = 0d;
+        count = 0d;
+    }
+
+}
+
+// Количество
+class CounterAgg implements Aggregator {
+
+    private double count = 0;
+
+    @Override
+    public void addValue(double value) {
+
+        count++;
+
+    }
+
+    @Override
+    public double getResult() {
+        return count;
+    }
+
+    @Override
+    public void reset() {
+        count = 0d;
+    }
+
+}
+
+// Максимальное значение
+class MaxAgg implements Aggregator {
+
+    private double maxValue = 0;
+
+    @Override
+    public void addValue(double value) {
+
+        if (maxValue < value) {
+            maxValue = value;
+        }
+
+    }
+
+    @Override
+    public double getResult() {
+        return maxValue;
+    }
+
+    @Override
+    public void reset() {
+        maxValue = 0d;
+    }
+
+}
+
+// Минимальное значение
+class MinAgg implements Aggregator {
+
+    private double minValue = 0;
+
+    @Override
+    public void addValue(double value) {
+
+        if (minValue > value) {
+            minValue = value;
+        }
+
+    }
+
+    @Override
+    public double getResult() {
+        return minValue;
+    }
+
+    @Override
+    public void reset() {
+        minValue = 0d;
+    }
+
+}
+
+// Сумма квадратов
+class SqdSumAgg implements Aggregator {
+
+    private double sqdSum = 0;
+    private double tmp;
+
+    @Override
+    public void addValue(double value) {
+
+        tmp = value;
+        sqdSum += tmp * tmp;
+
+    }
+
+    @Override
+    public double getResult() {
+        return sqdSum;
+    }
+
+    @Override
+    public void reset() {
+        sqdSum = 0d;
+        tmp = 0d;
+    }
+
 }
